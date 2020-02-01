41418736e223e0a933938d4bf3c45c9132e8593e
Yegor Kryukov <yegor.kryukov@gmail.com>
2020-02-01 09:20:20 +0500

    Added boiler plate log in flow

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,67 @@
 import 'package:flutter/material.dart';
-import 'meal_model.dart';
-import 'meal_list.dart';
-import 'menu.dart';
+import 'app.dart';
+import 'app_state_container.dart';
 
-
-// finish setting up firebase analytics from this link:
-// https://firebase.google.com/docs/flutter/setup
-
-
-void main() => runApp(TheApp());
-
-class TheApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Delivero',
-      theme: ThemeData(
-        primaryColor: Colors.orange,
-      ),
-      home: TheHomePage(title: 'Delivero'),
-    );
-  }
+void main()  {
+  runApp(new AppRootWidget());
 }
 
-class TheHomePage extends StatefulWidget {
-  TheHomePage({Key key, this.title}) : super(key: key);
 
-  final String title;
 
-  @override
-  _TheHomePageState createState() => _TheHomePageState();
-}
 
-class _TheHomePageState extends State<TheHomePage> {
-  List<Meal> initialMeals = []
-    ..add(Meal('Плов', 'Чайхона №1', 'Лучший плов в городе', '10.0'))
-    ..add(Meal('Самса', 'Чайхона №2', 'Лучшая самса в городе', '25.0'))
-    ..add(Meal('Burger', 'SteakHouse', 'The burgers of the house', '45.0'));
+// import 'package:flutter/material.dart';
+// import 'meal_model.dart';
+// import 'meal_list.dart';
+// import 'menu.dart';
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Container(
-          child: Center(
-        child: MealList(initialMeals),
-      )),
-      drawer: new Drawer(
-        child: TheAppMenuPage(),
-      ),
-    );
-  }
-}
+
+// // finish setting up firebase analytics from this link:
+// // https://firebase.google.com/docs/flutter/setup
+
+
+// void main() => runApp(TheApp());
+
+// class TheApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Delivero',
+//       theme: ThemeData(
+//         primaryColor: Colors.orange,
+//       ),
+//       home: TheHomePage(title: 'Delivero'),
+//     );
+//   }
+// }
+
+// class TheHomePage extends StatefulWidget {
+//   TheHomePage({Key key, this.title}) : super(key: key);
+
+//   final String title;
+
+//   @override
+//   _TheHomePageState createState() => _TheHomePageState();
+// }
+
+// class _TheHomePageState extends State<TheHomePage> {
+//   List<Meal> initialMeals = []
+//     ..add(Meal('Плов', 'Чайхона №1', 'Лучший плов в городе', '10.0'))
+//     ..add(Meal('Самса', 'Чайхона №2', 'Лучшая самса в городе', '25.0'))
+//     ..add(Meal('Burger', 'SteakHouse', 'The burgers of the house', '45.0'));
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body: Container(
+//           child: Center(
+//         child: MealList(initialMeals),
+//       )),
+//       drawer: new Drawer(
+//         child: TheAppMenuPage(),
+//       ),
+//     );
+//   }
+// }
